713255f42bee2f81dab8101e164d17bec61d1a79
User <user@Users-Air.phub.net.cable.rogers.com>
2020-09-01 18:41:36 -0400

    init commit

---
+++ organizer_app/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  organizer_app
+//
+//  Created by User on 2020-09-01.
+//  Copyright © 2020 aaronhertner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
