f31c353cd5cd0e85c0fcd988344fe3781667ae35
User <user@Users-Air.phub.net.cable.rogers.com>
2020-09-02 18:59:04 -0400

    began UI views

--- organizer_app/View Controllers/ViewController.swift
+++ organizer_app/View Controllers/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import os.log
 
-class ViewController: UIViewController {
-
+class ExpandedTaskViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Outlets
+    @IBOutlet weak var taskLabel : UILabel!
+    @IBOutlet weak var taskNameTextField : UITextField!
+    @IBOutlet weak var taskDetailsTextFIeld : UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        taskNameTextField.delegate = self
     }
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        taskNameTextField.resignFirstResponder()    //hides keyboard when return key pressed
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField){
+        taskLabel.text = taskNameTextField.text
+    }
 
 }
 
