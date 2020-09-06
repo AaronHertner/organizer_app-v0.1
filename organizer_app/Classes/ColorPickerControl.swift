798b00c97381a97ac7230d0650c16f54df685ea5
User <user@Users-Air.phub.net.cable.rogers.com>
2020-09-05 23:57:54 -0400

    additional work on color picke

---
+++ organizer_app/Classes/ColorPickerControl.swift
@@ -0,0 +1,21 @@
+//
+//  ColorPickerControl.swift
+//  organizer_app
+//
+//  Created by User on 2020-09-05.
+//  Copyright © 2020 aaronhertner. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
